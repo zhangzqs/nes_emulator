98156a76a3b70064a5c60fa3ca991660046bebb9
zzq <i@zhangzqs.cn>
2022-09-01 12:52:35 +0800

    Add: 添加BitSet工具类

--- lib/util.dart
+++ lib/util.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 extension IntExtension on int {
   String toHex([int len = 2]) => toUnsigned(16).toRadixString(16).padLeft(len, "0").toUpperCase();
 
@@ -21,4 +23,96 @@ class FlagBits<T extends Enum> {
   void set(T flag) => this[flag] = true;
   void reset(T flag) => this[flag] = false;
   void resetAll() => value = 0;
+}
+
+class BitSet {
+  final Uint8List _data;
+  final int _length;
+  BitSet(int length)
+      : _length = length,
+        _data = Uint8List((length / 8).floor() + 1);
+
+  Uint8List get rawData => _data;
+
+  void set(int i) {
+    int n = (i / 8).floor();
+    int offset = i % 8;
+    _data[n] |= 1 << offset;
+  }
+
+  void reset(int i) {
+    int n = (i / 8).floor();
+    int offset = i % 8;
+    if (_data[n] >> offset & 1 == 1) {
+      _data[n] &= ~(1 << offset);
+    }
+  }
+
+  void resetAll() {
+    for (int i = 0; i < _data.length; i++) {
+      _data[i] = 0;
+    }
+  }
+
+  bool get(int i) {
+    int n = (i / 8).floor();
+    int offset = i % 8;
+    return _data[n] >> offset & 1 == 1;
+  }
+
+  bool operator [](int i) => get(i);
+  void operator []=(int i, bool val) => val ? set(i) : reset(i);
+
+  int get length => _length;
+
+  BitSet operator <<(int n) {
+    BitSet bs = BitSet(length);
+    for (int i = 0; i < length - n; i++) {
+      bs[i + n] = this[i];
+    }
+    return bs;
+  }
+
+  BitSet operator >>(int n) {
+    BitSet bs = BitSet(length);
+    for (int i = 0; i < length - n; i++) {
+      bs[i] = this[i + n];
+    }
+    return bs;
+  }
+
+  @override
+  String toString() {
+    return Iterable.generate(length).map((i) => get(i).asInt()).toList().reversed.join();
+  }
+
+  BitSet subset(int start, int end) {
+    BitSet bs = BitSet(end - start);
+    for (int i = start; i < end; i++) {
+      bs[i - start] = this[i];
+    }
+    return bs;
+  }
+
+  void or(BitSet bs) {
+    for (int i = 0; i < bs.length; i++) {
+      this[i] = this[i] || bs[i];
+    }
+  }
+
+  static BitSet from(int n, int bitLength) {
+    BitSet bs = BitSet(bitLength);
+    for (int i = 0; i < bitLength; i++) {
+      bs[i] = (n >> i) & 1 == 1;
+    }
+    return bs;
+  }
+
+  int toInt() {
+    int num = 0;
+    for (int i = 0; i < length; i++) {
+      num |= (get(i).asInt() << i);
+    }
+    return num;
+  }
 }
---
+++ test/util_test.dart
@@ -0,0 +1,26 @@
+import 'package:nes_emulator/util.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('BitSet', () {
+    BitSet bs = BitSet(10);
+    expect(bs.length, 10);
+    expect(bs.rawData.length, 2);
+    for (int i = 0; i < 10; i++) {
+      expect(bs.get(i), isFalse);
+      bs.set(i);
+      expect(bs.get(i), isTrue);
+      bs.reset(i);
+      expect(bs.get(i), isFalse);
+    }
+    expect(bs.toString(), '0000000000');
+    bs[0] = true;
+    bs[9] = true;
+    expect(bs.toString(), '1000000001');
+    bs = bs >> 2;
+    expect(bs.toString(), '0010000000');
+    bs = bs << 1;
+    expect(bs.toString(), '0100000000');
+    expect(bs.subset(7, 10).toInt(), 2);
+  });
+}
