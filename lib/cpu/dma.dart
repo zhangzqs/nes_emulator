98fbe26ead135aba2db358e59d95ff7fd73a8f94
zzq <i@zhangzqs.cn>
2022-09-01 12:55:41 +0800

    Fix

---
+++ lib/cpu/dma.dart
@@ -0,0 +1,31 @@
+// BusAdapter getDmaControllerAdapter({
+//   required DmaController dmaController,
+//   required int targetPage,
+// }) {
+//   return DmaControllerAdapter(this, dmaController, targetPage);
+// }
+part of 'cpu.dart';
+
+/// 该控制器已集成到cpu内部
+class DmaControllerAdapter implements BusAdapter {
+  final CPU cpu;
+  final DmaController dmaController;
+  final U8 targetPage;
+  DmaControllerAdapter(this.cpu, this.dmaController, this.targetPage);
+
+  @override
+  bool accept(U16 address) => address == 0x4014;
+
+  @override
+  U8 read(U16 address) => throw UnsupportedError('DMA controller cannot be read');
+
+  @override
+  void write(U16 address, U8 value) {
+    // 启动DMA传输
+    final page = value; // 页号
+    // 开始拷贝
+    dmaController.transferPage(page, targetPage);
+    // 写入完毕需要更新剩余周期数, 之前的总周期若为奇数则等待513周期，偶数为514
+    cpu.stall += 513 + (cpu.totalCycles % 2);
+  }
+}
